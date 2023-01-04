69359dbc3d39a88a3052948250cd18420eef2032
cmackin9500 <casey@mackinnon.org>
2023-01-04 14:50:57 -0500

    Added some functionality but the score_board is not being updated properly

--- player.js
+++ player.js
@@ -17,20 +17,32 @@ class createPlayer {
     }
 }
 
-function firstThrow() {
+class scoreBoard extends createPlayer {
+    constructor(name) {
+        super(name);
+        this.score_board = Array(10).fill([0,0]);
+    }
+    set roundScore(roundInfo) {
+        this.score_board[roundInfo[0]][0] = roundInfo[1];
+        this.score_board[roundInfo[0]][1] = roundInfo[2];
+        console.log(this.score_board[roundInfo[0]])
+    }   
+}
+
+function pointsFromThrow() {
     return Math.floor(Math.random() * 11);
 }
 
-function secondThrow() {
-    return Math.floor(Math.random() * 11);
-}
-
-let player = new createPlayer('Peter');
+let player = new scoreBoard('Peter');
 
 for(let i=0; i < 10; i++) {
-    player.newScore = firstThrow();
-    player.newScore = secondThrow();
+    fT = pointsFromThrow();
+    sT = pointsFromThrow();
+    player.roundScore = [i,fT,sT];
+    player.newScore = fT
+    player.newScore = sT;
     console.log(`Score after round ${i+1} is ${player.score}`);
 }
 
 console.log(`Final score for ${player.name} is ${player.score}.`)
+console.log(player.score_board);
