39d52b836d51f598afab8236c5cfd5405e8ba287
cmackin9500 <casey@mackinnon.org>
2023-01-09 19:24:54 -0500

    Got the table running. Need major fixes

--- visual.js
+++ visual.js
@@ -1,19 +1,39 @@
-function createTable(tableData) {
-    var table = document.createElement('table');
-    var tableBody = document.createElement('tbody');
-  
-    tableData.forEach(function(rowData) {
-      var row = document.createElement('tr');
-  
-      rowData.forEach(function(cellData) {
-        var cell = document.createElement('td');
-        cell.appendChild(document.createTextNode(cellData));
+function createTable() {
+    
+    let table = document.getElementById('table');
+    let headerRow = document.createElement('tr');
+    
+    headerRow.appendChild(document.createElement("th")).appendChild(document.createTextNode("Round"));
+    for (let i=1; i<=10; i++) {
+        let header = document.createElement("th");
+        let textNode = document.createTextNode(i);
+        header.appendChild(textNode);
+        headerRow.appendChild(header);
+    }
+    table.appendChild(headerRow);
+    
+    let row = document.createElement('tr');
+    
+    for (let i=0; i<=10; i++) {
+        let cell = document.createElement('td');
+        let textNode = document.createTextNode(0);
+
+        if (i === 0) {
+            textNode = document.createTextNode("Score");
+        }
+
+        cell.appendChild(textNode);
         row.appendChild(cell);
-      });
-  
-      tableBody.appendChild(row);
-    });
-  
-    table.appendChild(tableBody);
+    }
+    table.appendChild(row);
+
     document.body.appendChild(table);
+    //columns = document.getElementById("th").setAttribute("rowspan",2);
+}
+
+function updateValue(player) {
+    if (player.turn === 1) {
+        var table = document.getElementById('table');
+        table.rows[1].cells[(player.i)+1].innerHTML = player.score_board[player.i][0]+player.score_board[player.i][1];
+    }
 }
